fc8b79f90639b297c13d96da93f562aa4b05c420
Renan Freitas <renanfsouza135@gmail.com>
2025-08-09 19:32:58 -0300

    update

---
+++ inicio/ex01.js
@@ -0,0 +1,17 @@
+// Tipo booleano
+let num = ' ' // false para vazio
+
+if (!num.trim()) {
+    console.log('vazio')
+} else {
+    console.log('preenchido')
+}
+
+// Array
+let lista = ['renan', 17, 1.78]
+
+lista.push('man') // adiciona o item ao final
+console.log(lista)
+
+lista.pop()
+console.log(lista) // remove o ultimo item
