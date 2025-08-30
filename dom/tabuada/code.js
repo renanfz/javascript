438f2164d3c368223e44f7cfafd983bb27fdc27e
Renan Freitas <renanfsouza135@gmail.com>
2025-08-30 15:35:52 -0300

    conclusao desafio

--- dom/tabuada/code.js
+++ dom/tabuada/code.js
@@ -1,6 +1,22 @@
-let tabela = document.querySelector('table')
+
 
 
 function calcularTabuada() {
+    let tabela = document.querySelector('table')
+    let numeroEscolhido = document.querySelector('input').value
+    let tituloTabela = document.getElementById('numeroTabuada')
+    tituloTabela.innerText = numeroEscolhido
     
+    for (let n = 0; n <= 10; n++) {
+        let linha = document.createElement('tr')
+        let celulaExpressao = document.createElement('td')
+        let celulaResultado = document.createElement('td')
+
+        celulaExpressao.innerText = `${n} x ${numeroEscolhido} = `
+        celulaResultado.innerText = (n * numeroEscolhido)
+
+        linha.appendChild(celulaExpressao)
+        linha.appendChild(celulaResultado)
+        tabela.appendChild(linha)
+    }
 }
