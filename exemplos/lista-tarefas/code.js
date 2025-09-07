2f52321f32469f0b2af7c87bce2f70af95369eee
Renan Freitas <renanfsouza135@gmail.com>
2025-09-07 20:30:59 -0300

    update no projeto!!!

---
+++ exemplos/lista-tarefas/code.js
@@ -0,0 +1,10 @@
+// Elementos
+let bntOpenModal = document.getElementById('openModal')
+let modal = document.querySelector('dialog')
+
+
+bntOpenModal.addEventListener('click', () => {
+    modal.showModal();
+})
+
+lucide.createIcons()
